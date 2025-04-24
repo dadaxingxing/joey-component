547ea10539a51e47be87efe07e54bc606e1d1246
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-04-23 17:41:39 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Joey_component from './Joey_component'
 
 function App() {
 
   return (
-    <div></div>
+    <Joey_component/>
   )
 }
 
--- src/Joey_component.jsx
+++ src/Joey_component.jsx
@@ -1,1 +1,12 @@
-import './Joey_compoent.css';
+import './Joey_component.css'
+import { useState } from 'react'
+
+function Joey_component() {
+    
+
+    return (
+        <div></div>
+    )
+};
+
+export default Joey_component;
